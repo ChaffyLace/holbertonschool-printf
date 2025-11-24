77bb45cf5dc00f9f864f2140843b0238a8768ae0
Nikola <vediumtv93@gmail.com>
2025-11-24 14:21:30 +0000

    task0

---
+++ _printf.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: format string
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int i = 0;
+    int count = 0;
+
+    if (format == NULL)
+	return(0)
+
+    /* 2. Initialiser va_list */
+    va_start(args, format);
+
+    /* 3. Parcourir format */
+    while (format[i] != '\0')
+    {
+        if(format[i] != '%')
+	{
+		write(1, &format[i], 1);
+        	count++;
+	}
+        else
+	{
+		i++;
+
+		if (format[i] == 'c')
+		}
+
+		}
+		else if (format[i] == 's')
+		{
+
+		}
+		elese if(format[i] == '%')
+		{
+		
+		}
+
+
+        /* 6. Mettre à jour count */
+
+        i++;
+    }
+
+    /* 7. Terminer va_list */
+    va_end(args);
+
+    /* 8. Retourner le nombre de caractères affichés */
+    return (count);
+}
