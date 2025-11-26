476f94f2ea565c66fde82f74c16029661fab88ae
nikolag1jic93430-lab <nikolag1jic93430@gmail.com>
2025-11-26 10:13:08 +0100

    espace excédentaire corriger

--- _printf.c
+++ _printf.c
@@ -9,52 +9,52 @@
 int _printf(const char *format, ...)
 {
     va_list args;
-    int i = 0;
-    int count = 0;
+    int i = 0, count = 0;
 
     if (format == NULL)
-	return(0)
+        return (0);
 
-    /* 2. Initialiser va_list */
     va_start(args, format);
 
     while (format[i] != '\0')
     {
-        if(format[i] != '%')
-	{
-		write(1, &format[i], 1);
-        	count++;
-	}
+        if (format[i] != '%')
+        {
+            write(1, &format[i], 1);
+            count++;
+            i++;
+            continue;
+        }
+		
+        i++; 
+
+        if (format[i] == 'c')
+        {
+            char c = va_arg(args, int);
+            write(1, &c, 1);
+            count++;
+        }
+        else if (format[i] == 's')
+        {
+            char *s = va_arg(args, char *);
+            if (s == NULL)
+                s = "(null)";
+            for (int j = 0; s[j] != '\0'; j++)
+            {
+                write(1, &s[j], 1);
+                count++;
+            }
+        }
+        else if (format[i] == '%')
+        {
+            write(1, "%", 1);
+            count++;
+        }
         else
-	{
-		i++;
-
-		if (format[i] == 'c')
-
-		{
-    			char c = va_arg(args, int);
-    			write(1, &c, 1);
-    			count++;
-
-		}
-		else if (format[i] == 's')
-		{
-			char *s = va_arg(args, char *);
-			if (s == NULL)
-				s = "(null)";
-			int j = 0;
-			while (s[j] != '\0')
-			{
-				write(1, &s[j], 1);
-				count++;
-				j++;
-			}
-		}
-		else if(format[i] == '%')
-		{
-			write(1, "%", 1);
-			count++;
-		}
+        {
+            write(1, &format[i], 1);
+            count++;
+        }
 
         i++;
     }
@@ -63,3 +63,4 @@ int _printf(const char *format, ...)
 
     return (count);
 }
+
