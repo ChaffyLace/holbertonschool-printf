0dbe37bf8f03d54452b7962ad362c9fc3cff049b
nikolag1jic93430-lab <nikolag1jic93430@gmail.com>
2025-11-26 10:23:16 +0100

    correction du code precedent

--- _printf.c
+++ _printf.c
@@ -8,59 +8,45 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, count = 0;
+	va_list args;
+	int i = 0, count = 0;
 
-    if (format == NULL)
-        return (0);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format[i] != '\0')
-    {
-        if (format[i] != '%')
-        {
-            write(1, &format[i], 1);
-            count++;
-            i++;
-            continue;
-        }
-		
-        i++; 
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		else
+		{
+			i++;
+			
+			if (format[i] == 'c')
+				count += print_char(args);
 
-        if (format[i] == 'c')
-        {
-            char c = va_arg(args, int);
-            write(1, &c, 1);
-            count++;
-        }
-        else if (format[i] == 's')
-        {
-            char *s = va_arg(args, char *);
-            if (s == NULL)
-                s = "(null)";
-            for (int j = 0; s[j] != '\0'; j++)
-            {
-                write(1, &s[j], 1);
-                count++;
-            }
-        }
-        else if (format[i] == '%')
-        {
-            write(1, "%", 1);
-            count++;
-        }
-        else
-        {
-            write(1, &format[i], 1);
-            count++;
-        }
+			else if (format[i] == 's')
+				count += print_string(args);
 
-        i++;
-    }
+			else if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+			else
+			{
+				return (-1);
+			}
+		}
 
-    va_end(args);
+		i++;
+	}
 
-    return (count);
+	va_end(args);
+	return (count);
 }
-
