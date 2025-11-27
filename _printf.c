60b5ada78945bcd05381d3c1ff66c41998a34e96
nikolag1jic93430-lab <nikolag1jic93430@gmail.com>
2025-11-27 13:20:47 +0100

    modification pour la task1
    Added error handling for null terminator in format string.

--- _printf.c
+++ _printf.c
@@ -25,17 +25,25 @@ int _printf(const char *format, ...)
         if (format[i] == '%')
         {
             i++;
+
             if (format[i] == '\0')
             {
                 va_end(args);
                 return -1;
             }
+
             if (format[i] == 'c')
                 count += print_char(args);
+
             else if (format[i] == 's')
                 count += print_string(args);
+
+            else if (format[i] == 'd' || format[i] == 'i')
+                count += print_int(va_arg(args, int));
+
             else if (format[i] == '%')
                 count += _putchar('%');
+
             else
             {
                 count += _putchar('%');
